5a174de1b80ff6e63f11eb0e39ec666b91580f0a
davis <davis.fusco.20@gmail.com>
2022-09-28 22:46:14 +0200

    fix: fixed post requests

--- web 2/src/main/java/ch/supsi/webapp/TicketServlet.java
+++ web 2/src/main/java/ch/supsi/webapp/TicketServlet.java
@@ -1,32 +1,42 @@
 package ch.supsi.webapp;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 @WebServlet(value = "/tickets")
 public class TicketServlet extends HttpServlet {
-    private ArrayList<Ticket> tickets = new ArrayList<>();
+    private final ArrayList<Ticket> tickets = new ArrayList<>();
     ObjectMapper mapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        // res.getWriter().println("Hello world!!");
-        // Ticket ticket = mapper.readValue(new File("data.json"), Ticket.class);
-        tickets = mapper.readValue(new File("data/data.json"), mapper.getTypeFactory().constructCollectionType(ArrayList.class, Ticket.class));
-        res.getWriter().println(tickets);
+        res.getWriter().println(mapper.writeValueAsString(tickets));
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        Ticket ticket = mapper.readValue(req.getInputStream(), Ticket.class);
-        tickets.add(ticket);
-        mapper.writeValue(new File("data/data.json"), tickets);
+        String type = req.getContentType();
+        Ticket ticket = null;
+
+        if (type != null) {
+            if (type.equals("application/json") || type.equals("plain/text")) {
+                ticket = mapper.readValue(req.getInputStream(), Ticket.class);
+                tickets.add(ticket);
+            } else if (type.equals("application/x-www-form-urlencoded")) {
+                String title = req.getParameter("title");
+                String description = req.getParameter("description");
+                String author = req.getParameter("author");
+                ticket = new Ticket(title, description, author);
+                tickets.add(ticket);
+            }
+        }
+
+        res.getWriter().println(mapper.writeValueAsString(ticket));
     }
 }
