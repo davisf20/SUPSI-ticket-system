68a7260c44671a5f48f27c93bf72c226286f02d7
davis <davis.fusco.20@gmail.com>
2022-10-09 21:38:20 +0200

    feat: added implemented Patch method

--- web 2/src/main/java/ch/supsi/webapp/TicketServlet.java
+++ web 2/src/main/java/ch/supsi/webapp/TicketServlet.java
@@ -1,6 +1,7 @@
 package ch.supsi.webapp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +14,15 @@ import java.util.ArrayList;
 public class TicketServlet extends HttpServlet {
     private final ArrayList<Ticket> tickets = new ArrayList<>();
     ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getMethod().equalsIgnoreCase("PATCH")){
+            doPatch(req, resp);
+        } else {
+            super.service(req, resp);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
@@ -107,6 +117,39 @@ public class TicketServlet extends HttpServlet {
             } else {
                 res.setStatus(404);
                 res.getWriter().println("Ticket not found");
+            }
+        } else {
+            res.setStatus(400);
+            res.getWriter().println("Bad request");
+        }
+    }
+
+    protected void doPatch(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        if (req.getPathInfo() != null) {
+            String type = req.getContentType();
+            if (type != null) {
+                if (type.equals("application/json")) {
+                    int id = Integer.parseInt(req.getPathInfo().substring(1));
+                    Ticket ticket = tickets.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+                    if (ticket != null) {
+                        Ticket newTicket = mapper.readValue(req.getInputStream(), Ticket.class);
+                        if (newTicket.getTitle() != null) {
+                            ticket.setTitle(newTicket.getTitle());
+                        }
+                        if (newTicket.getDescription() != null) {
+                            ticket.setDescription(newTicket.getDescription());
+                        }
+                        if (newTicket.getAuthor() != null) {
+                            ticket.setAuthor(newTicket.getAuthor());
+                        }
+                        res.setStatus(200);
+                        res.setHeader("Content-Type", "application/json");
+                        res.getWriter().println(mapper.writeValueAsString(ticket));
+                    } else {
+                        res.setStatus(404);
+                        res.getWriter().println("Ticket not found");
+                    }
+                }
             }
         } else {
             res.setStatus(400);
