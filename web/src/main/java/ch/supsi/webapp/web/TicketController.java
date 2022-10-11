1b68e1fa6baa60af4f1fe2abd194ba4342e6f09c
Davis Fusco <davis.fusco.20@gmail.com>
2022-10-11 15:57:23 +0200

    fix: fixed requests with id

--- web/src/main/java/ch/supsi/webapp/web/TicketController.java
+++ web/src/main/java/ch/supsi/webapp/web/TicketController.java
@@ -2,15 +2,15 @@ package ch.supsi.webapp.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+@RestController
 public class TicketController {
-    private List<Ticket> tickets = new ArrayList<>();
+    private final List<Ticket> tickets = new ArrayList<>();
 
     @RequestMapping(value = "/tickets", method = RequestMethod.POST)
     public ResponseEntity<Ticket> post(@RequestBody Ticket ticket) {
@@ -23,8 +23,8 @@ public class TicketController {
         return tickets;
     }
 
-    @RequestMapping(value="/tickets/*", method= RequestMethod.GET)
-    public ResponseEntity<Ticket> get(int id) {
+    @RequestMapping(value="/tickets/{id}", method= RequestMethod.GET)
+    public ResponseEntity<Ticket> get(@PathVariable int id) {
         Ticket ticket = tickets.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
         if (ticket != null) {
             return new ResponseEntity<Ticket>(ticket, HttpStatus.OK);
@@ -33,8 +33,8 @@ public class TicketController {
         }
     }
 
-    @RequestMapping(value="/tickets/*", method= RequestMethod.PUT)
-    public ResponseEntity<Ticket> put(int id, @RequestBody Ticket newTicket) {
+    @RequestMapping(value="/tickets/{id}", method= RequestMethod.PUT)
+    public ResponseEntity<Ticket> put(@PathVariable int id, @RequestBody Ticket newTicket) {
         Ticket ticket = tickets.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
         if (ticket != null) {
             ticket.setTitle(newTicket.getTitle());
@@ -46,15 +46,14 @@ public class TicketController {
         }
     }
 
-    @RequestMapping(value="/tickets/*", method= RequestMethod.DELETE)
-    public ResponseEntity<Object> delete(int id) {
+    @RequestMapping(value="/tickets/{id}", method= RequestMethod.DELETE)
+    public ResponseEntity<Map<String, Object>> delete(@PathVariable int id) {
         Ticket ticket = tickets.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
         if (ticket != null) {
             tickets.remove(ticket);
-            // Returns a JSON indicating whether the removal was successful and status code = 200
-            return new ResponseEntity<Object>(HttpStatus.OK);
+            return new ResponseEntity<Map<String, Object>>(Map.of("success", true), HttpStatus.OK);
         } else {
-            return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
